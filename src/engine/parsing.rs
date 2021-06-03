938ed3c29a426529a355ca593bb92b0b07241461
Zane Sterling <sterling.zane@gmail.com>
2021-06-02 23:45:01 -0700

    Pull the Result out of Parsing.val
    Users should instead use the `?` operator between calls as shown
    in the example at the head of the file.

--- src/engine/parsing.rs
+++ src/engine/parsing.rs
@@ -1,12 +1,12 @@
 // ```
 // Parsing::new(input).try_one([
+//     |p| Err(Error("oh no, an error!")),
 //     |p| {
 //         let p1 = p.parse_literal()?;
 //         let p2 = p1.skip("+")?.parse_literal()?;
-//         Ok(Expr::Plus(Box::new(p1.val), Box::new(e2.val)))
+//         Ok(p2.replace(Expr::Plus(Box::new(p1.val), Box::new(e2.val))))
 //     },
-//     |p| Ok(()),
-// ])
+// ])?;
 // ```
 
 use std::num;
@@ -22,9 +22,7 @@ where
     // copy this left and right.
     s: String,
     i: usize,
-    // TODO: I should put Result on the outside and use question marks. That
-    // way I don't have to check that val.is_okay at start of every call.
-    val: Result<T, Error>,
+    val: T,
 }
 
 impl Parsing<()> {
@@ -32,13 +30,13 @@ impl Parsing<()> {
         Parsing {
             s: s,
             i: 0,
-            val: Ok(()),
+            val: (),
         }
     }
 }
 
 impl<T: Clone> Parsing<T> {
-    fn replace<T2: Clone>(&self, val: Result<T2, Error>) -> Parsing<T2> {
+    fn replace<T2: Clone>(self, val: T2) -> Parsing<T2> {
         Parsing {
             s: self.s.clone(),
             i: self.i,
@@ -46,43 +44,31 @@ impl<T: Clone> Parsing<T> {
         }
     }
 
-    fn try_one<T2: Clone>(
+    fn try_one<T2: Clone, F: FnOnce(Parsing<T>) -> Result<Parsing<T2>, Error>>(
         self,
-        methods: Vec<Box<dyn Fn(&mut Parsing<T>) -> Result<T2, Error>>>,
-    ) -> Parsing<T2> {
-        if self.val.is_err() {
-            let err = Err(self.val.clone().err().unwrap());
-            return self.replace(err);
-        }
-
+        methods: Vec<F>,
+    ) -> Result<Parsing<T2>, Error> {
         for method in methods {
-            let mut p = self.clone();
-            let result = method(&mut p);
+            let result = method(self.clone());
             if result.is_ok() {
-                return p.replace(result);
+                return result;
             }
         }
 
-        let err = Error(format!(
+        Err(Error(format!(
             "No method worked parsing at {}",
             self.s.get(self.i..).unwrap_or("")
-        ));
-        self.replace(Err(err))
+        )))
     }
 
-    fn skip(mut self, s: &str) -> Parsing<T> {
-        if self.val.is_err() {
-            return self;
-        }
-
-        let rest = self.s.get(self.i..);
-        if rest.map(|r| r.starts_with(s)).unwrap_or(false) {
+    fn skip(mut self, s: &str) -> Result<Parsing<T>, Error> {
+        if self.s[self.i..].starts_with(s) {
             self.i += s.len();
-            return self;
+            Ok(self)
+        } else {
+            let err = format!("Expected \"{}\" but found \"{}\" instead", s, self.s);
+            Err(Error(err))
         }
-
-        let err = format!("Expected \"{}\" but found \"{}\" instead", s, self.s);
-        self.replace(Err(Error(err)))
     }
 
     fn parse_int(mut self) -> Result<Parsing<i64>, Error> {
@@ -98,7 +84,7 @@ impl<T: Clone> Parsing<T> {
         } else {
             let x = self.s[self.i..int_end].parse::<i64>()?;
             self.i = int_end;
-            Ok(self.replace(Ok(x)))
+            Ok(self.replace(x))
         }
     }
 }
@@ -117,22 +103,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_skip() {
-        let p = Parsing::new("foo".to_string());
-        let p = p.skip("fo");
-        assert_eq!(p.val.is_ok(), true);
+    fn test_skip() -> Result<(), Error> {
+        let p = Parsing::new("foo".to_string()).skip("fo")?;
         assert_eq!(p.i, 2);
         assert_eq!(p.s, "foo");
+        Ok(())
     }
 
     #[test]
-    fn test_parse_int() -> Result<(), Error> {
+    fn test_parse_int_respects_end_of_string() -> Result<(), Error> {
         let p = Parsing::new("123".to_string()).parse_int()?;
-        assert_eq!(p.val?, 123);
+        assert_eq!(p.val, 123);
         assert_eq!(p.i, 3);
+        Ok(())
+    }
 
+    #[test]
+    fn test_parse_int_respects_alpha_chars() -> Result<(), Error> {
         let p = Parsing::new("456foo".to_string()).parse_int()?;
-        assert_eq!(p.val?, 456);
+        assert_eq!(p.val, 456);
         assert_eq!(p.i, 3);
         Ok(())
     }
